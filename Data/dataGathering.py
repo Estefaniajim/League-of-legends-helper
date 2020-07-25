2f8a31b2ce3e34f1ce36a78c61feb5a59878c69e
Estefania Jimenez Garcia <31485312+Estefaniajim@users.noreply.github.com>
2020-07-25 03:29:49 -0500

    Update dataGathering.py

--- Data/dataGathering.py
+++ Data/dataGathering.py
@@ -7,7 +7,6 @@ import pandas as pd
 # Getting the api key from .env
 load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
-print(APIKey)
 # Getting the data from a json file to a string
 matchData = requests.get('http://canisback.com/matchId/matchlist_na1.json')
 matchesID = json.loads(matchData.text)
@@ -17,11 +16,30 @@ def getMatchID():
         getInfoWithMatchID(matchID)
 
 def getInfoWithMatchID(matchID):
-    print(matchID)
-    URL = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(matchID) + "?api_key=" + str(APIKey)
+    URL = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(matchID) + "?api_key=" + str(API_KEY)
     info = requests.get(URL)
-    return info
+    info = json.loads(info.text)
+    for x in range(10):
+        accountId = info["participantIdentities"][x]["player"]["accountId"]
+        kills = info["participants"][x]["stats"]["kills"]
+        deaths = info["participants"][x]["stats"]["deaths"]
+        assists = info["participants"][x]["stats"]["assists"]
+        creepsPerMin = info["participants"][x]["timeline"]["creepsPerMinDeltas"]["10-20"]
+        goldPerMin = info["participants"][x]["timeline"]["goldPerMinDeltas"]["10-20"]
+        lane = info["participants"][x]["lane"]
+        tier,rank = getRankedPosition(accountId)
 
-print(getInfoWithMatchID(matchesID[1]))
+
+def getRankedPosition(accountId):
+    URL = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + str(accountId) + "?api_key=" + str(API_KEY)
+    info = requests.get(URL)
+    info = json.loads(info.text)
+    tier = info[0]["tier"]
+    rank = info[0]["rank"]
+    return tier,rank
+
+print(getRankedPosition("eWxokGvNaAQxQWg_ZvCxtpZFiM3Hcmu8kwvPYK9-tY4h9FE"))
+
+
 
 
