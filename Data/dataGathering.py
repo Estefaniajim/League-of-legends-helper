2cb2921e3e5a99baacaf911c03108db205411f8d
Estefania Jimenez Garcia <31485312+Estefaniajim@users.noreply.github.com>
2020-07-25 16:43:56 -0500

    Update dataGathering.py

--- Data/dataGathering.py
+++ Data/dataGathering.py
@@ -10,6 +10,14 @@ API_KEY = os.getenv("RIOT_API_KEY")
 # Getting the data from a json file to a string
 matchData = requests.get('http://canisback.com/matchId/matchlist_na1.json')
 matchesID = json.loads(matchData.text)
+pdata = pd.DataFrame(pd.DataFrame({"kills":[],
+                      "deaths":[],
+                      "assists":[],
+                      "creepsPerMin":[],
+                      "goldPerMin":[],
+                      "lane":[],
+                      "tier":[],
+                      "rank":[]}))
 
 def getMatchID():
     for matchID in matchesID:
@@ -19,9 +27,15 @@ def getRankedPosition(accountId):
     URL = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + str(accountId) + "?api_key=" + str(API_KEY)
     info = requests.get(URL)
     info = json.loads(info.text)
-    tier = info[0]["tier"]
-    rank = info[0]["rank"]
-    return tier,rank
+    while True:
+        try:
+            tier = info[0]["tier"]
+            rank = info[0]["rank"]
+            return tier,rank
+        except:
+            tier = "Unranked"
+            rank = 0
+            return tier,rank
 
 def getInfoWithMatchID(matchID):
     URL = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(matchID) + "?api_key=" + str(API_KEY)
@@ -34,5 +48,17 @@ def getInfoWithMatchID(matchID):
         assists = info["participants"][x]["stats"]["assists"]
         creepsPerMin = info["participants"][x]["timeline"]["creepsPerMinDeltas"]["10-20"]
         goldPerMin = info["participants"][x]["timeline"]["goldPerMinDeltas"]["10-20"]
-        lane = info["participants"][x]["lane"]
+        lane = info["participants"][x]["timeline"]["lane"]
         tier,rank = getRankedPosition(accountId)
+        print(accountId)
+        pdata.append(pd.DataFrame({"kills":[kills],
+                      "deaths":[deaths],
+                      "assists":[assists],
+                      "creepsPerMin":[creepsPerMin],
+                      "goldPerMin":[goldPerMin],
+                      "lane":[lane],
+                      "tier":[tier],
+                      "rank":[rank]}))
+# getInfoWithMatchID(3501821963)
+# print(pdata.head())
+
