8a7a69bb191388b5b858e834c3a9dff64e6184db
Estefania Jimenez Garcia <31485312+Estefaniajim@users.noreply.github.com>
2020-07-26 11:43:46 -0500

    updated model.py

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import app
 import Data.dataAnalysis as da
 import Data.liveDataLAN as lan
 import Data.liveDataNA as na
+import time
 
 #summonerName,server,lane = app.getUser()
 
@@ -15,15 +16,26 @@ def getDataServer(server,summonerName):
         tier,rank = na.getRankedPosition(summonerId)
         return summonerId,tier,rank
 
-def refreshData():
-    summonerId,tier,rank = getDataServer()
+def refreshData(lane,server,summonerName,creepsPerMin,goldPerMin):
     while True:
         try:
+            summonerId,tier,rank = getDataServer(server,summonerName)
             if server == "LAN":
-                gameTime = lan.gettingLiveScores(summonerId)
+                # gameTime = lan.gettingLiveScores(summonerId)
+                gameTime = 120
             else:
                 gameTime = na.gettingLiveScores(summonerId)
             creepsPerMin, goldPerMin = da.gettingAvgScores(gameTime,lane,tier,rank)
-            return creepsPerMin,goldPerMin
+            time.sleep(60)
         except:
-            break
+            print("Matched Ended")
+            print("Your Score should look like")
+            deaths = da.getAvgDeaths(lane,tier,rank)
+            kills = da.getAvgKills(lane,tier,rank)
+            assists = da.getAvgAssists(lane,tier,rank)
+            wardsKilled = da.getAvgWardsKilled(lane,tier,rank)
+            wardsPlaced = da.getAvgWardsPlaced(lane,tier,rank)
+            print("Your KDA: "+ str(kills)+"/"+str(deaths)+"/"+str(assists))
+            print("Your wards placed: "+ str(wardsPlaced)+ " yes, wards are important even if you are not a support")
+            print("Your wads killed: "+ str(wardsKilled)+ "yes, even killing wards is important")
+            return deaths,kills,assists,wardsKilled,wardsPlaced
